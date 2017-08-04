eb8cdb39b2be0522238672fd4393777358ea664a
Eduardo Luz <eduluz1976@gmail.com>
2017-08-04 08:13:51 -0700

    demo

---
+++ demo/test_01.php
@@ -0,0 +1,41 @@
+<?php
+
+include_once('../vendor/autoload.php');
+include_once('../src/KDB.php');
+include_once('../src/KTag.php');
+include_once('../src/KVar.php');
+include_once('../src/KFormula.php');
+include_once('../src/KException.php');
+
+use xcranium\kdb\KDB;
+
+KDB::createNewDB("teste1")
+->addKTag("deal")
+->addKTag("grant")
+->addKVar("idade", ["formula" => '@current_date_sub($data_nascimento)', 'tags'=>['deal']])
+->addKVar("data_nascimento", ["title" => "Data de nascimento", "type" => "date"])
+->addKVar("elegivel_desconto",["tags" => ["deal"], "title" => "Elegivel ao desconto", "formula" => '($idade > 60)'])
+->addKVar("eh_cidadao",["title" => "Voce eh cidadao?", "type"=>"yes_no"])
+->addKVar("elegivel_grant",["title"=>"Elegivel a um grant", "tags"=>["grant"], "formula" => '($idade > 65) AND ($eh_cidadao == “yes”)' ])
+->updateIndex();
+
+
+
+$x = KDB::get("teste1");
+echo "<pre>";
+//$x->setValue('idade', 37);
+//$x->setValue('eh_cidadao', 'yes');
+$x->setValue('data_nascimento', 'xxxx');
+
+print_r($x->getMetaVar('idade'));
+echo "<hr>";
+print_r($x->queryTag('deal'));
+echo "\n\n";
+//print_r($x->getValue('elegivel_desconto',true));
+print_r($x->getValue('elegivel_grant',true));
+//print_r($x->getValue('idade',true));
+echo "\n\n";
+print_r($x->evaluate());
+echo "\n\n\n\n";
+print_r(\xcranium\kdb\KFormula::$vars);
+
